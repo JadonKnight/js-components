7c4c61da4e6175ccc86fe80bb608fce34696af32
Jadon Knight <Jadon.Knight@outlook.com>
2022-08-20 12:14:21 +1000

    Add module exports for node support

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 import { createElement } from './create-component.js';
 import { buttonComponent } from './buttonComponent.js';
+
+// Experimenting with using this module in node
+module.exports = { createElement: createElement, buttonComponent: buttonComponent };
